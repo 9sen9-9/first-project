ad30dcaafa1b0976e9e3d6b02c32e394a7d3589d
9sen9-9 <sen9_69@mail.ru>
2024-10-24 18:03:34 +0300

    hw2

--- src/Main.java
+++ src/Main.java
@@ -2,48 +2,66 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-    var dog = 8.0;
-    var cat = 3.6;
-    var paper = 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-    dog = dog + 4;
-    cat = cat + 4;
-    paper = paper + 4;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-    dog = dog - 3.5;
-    cat = cat - 1.6;
-    paper = paper - 7639;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-    var friend = 19;
-        System.out.println(friend);
-    friend = friend * 2;
-        System.out.println(friend);
-    friend = friend / 7;
-        System.out.println(friend);
-    var frog = 3.5;
-        System.out.println(frog);
-    frog = frog * 10;
-        System.out.println(frog);
-    frog = frog / 3.5;
-        System.out.println(frog);
-    frog = frog + 4;
-        System.out.println(frog);
-    var boxer1 = 82.7;
-    var boxer2 = 78.2;
-    var common = boxer1 % boxer2;
-        System.out.println(common);
-    var AllTime = 640;
-    var employee = 8;
-    var total = AllTime / employee;
-        System.out.println(total);
-    var x = 94;
-    x = x + total;
-        System.out.println(x);
-    x = x % employee;
-        System.out.println(x);}
+        System.out.println("Task1");
+        int[] f = {300, 600, 900, 1200, 1500};
+        int total = 0;
+        for (int value : f) {
+            total += value;
+        }
+            System.out.println("Сумма трат за месяц составила" + total + "рубли");
+
+
+
+        System.out.println("Task2");
+        int[] f1 = {300, 600, 900, 1200, 1500};
+        int min = f1[0];
+        int max = f1[0];
+        for (int value : f1) {
+            if (value < min) {
+                min = value;
+            }
+            if (value > max) {
+                max = value;
+
+            }
+        }
+        System.out.println("Минимальная сумма трат за день составила" + min + "рублей");
+        System.out.println("Максимальная сумма трат за день составила" + max + "рублей");
+
+        System.out.println("Task3");
+        int[] f2 = {300, 600, 900, 1200, 1500};
+        int total1 = 0;
+        for (int value1 : f2) {
+            total1 += value1;
+        }
+            double average = total1 / f2.length;
+            System.out.println("Сумма трат  за день составила"+average+"рублей");
+
+        System.out.println("Task4");
+        char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        for (int i = reverseFullName.length-1;i>=0 ; i--) {
+            System.out.print(reverseFullName[i]);
+
+        }
+
+
+        }
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
